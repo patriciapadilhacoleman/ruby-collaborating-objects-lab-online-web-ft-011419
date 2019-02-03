8f1aa74b09165dacd5629c370ad4b0aa8f14e909
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-02-03 18:03:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,13 +11,13 @@ class Song
   
   def self.new_by_filename(filename)
     
-    artist_song = filename.split('-')
+    artist_song = filename.split(' - ')
     artist = Artist.new(artist_song[0]) 
     song = self.new(artist_song[1])
    
     song.artist = artist.save
     artist.add_song(song)
-    song
+    song 
     
   end
   
